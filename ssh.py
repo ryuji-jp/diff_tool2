d16aab45f9ddd7886756f71d0b7750e5069ca170
ryuji-jp <61535783+ryuji-jp@users.noreply.github.com>
2021-08-12 08:44:14 +0900

    Add files via upload

--- ssh.py
+++ ssh.py
@@ -1,5 +1,6 @@
 from netmiko.ssh_autodetect import SSHDetect
 from netmiko.ssh_dispatcher import ConnectHandler
+from netmiko import Netmiko
 import datetime
 import os
 import json
@@ -30,23 +31,24 @@ def ssh_log(device):
   #ssh接続できない時のエラーは無視します
   try:
     # 自動検出
-    guesser = SSHDetect(**remote_device)
-    best_match = guesser.autodetect()
+    #guesser = SSHDetect(**remote_device)
+    #best_match = guesser.autodetect()
 
     # 検出結果のデバッグ出力
-    print("[情報] device_type: " + best_match)
-    print("[情報] "+device+"実行中!!!")
+    #print("[情報] device_type: " + best_match)
+    #print("[情報] "+device+"実行中!!!")
 
     # 自動検出した device_type を再設定する
-    remote_device['device_type'] = best_match
-    connection = ConnectHandler(**remote_device)
+    #remote_device['device_type'] = best_match
+    connection = Netmiko(**remote_device)
     connection.enable() #enableパスワード入力
+    print("[情報] "+device+"実行中!!!")
 
     # コマンド実行結果の出力
     #print(connection.send_command('show clock\nshow run'),file=f)
 
     #command = open('input/'+remote_device['command'],'r')
-    command = open('input/'+remote_device_command["command"],'r')
+    command = open('input/command/'+remote_device_command["command"],'r')
     command_list = command.readlines()
 
     for i in range(len(command_list)):   # インデックス番号 0 から順に要素をスライスします。
@@ -67,7 +69,7 @@ def ssh_log(device):
     connection.disconnect()
 
   except :
-    print("[エラー] "+device+" SSH接続できません。対象機器に不備があるか、input/device配下ののjsonファイルに不備があります")
+    print("[エラー] "+device+" SSH接続できません。次の理由で接続できていません。netmikoが対象機器に対応していない。対象機器に不備がある。input/device配下ののjsonファイルに不備がある。")
     
     #csv書き込み
     with open('output/取得結果.csv','a',newline="") as c:
